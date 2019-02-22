cb2dae21d314c2e87100910c10111f98f058abbd
RED-GATE\david.you <david.you@red-gate.com>
2019-02-22 11:28:39 -0800

    modified sproc and added new data

--- Data/Person.ContactType_Data.sql
+++ Data/Person.ContactType_Data.sql
@@ -1,4 +1,7 @@
 SET IDENTITY_INSERT [Person].[ContactType] ON
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1038, N'CaterpillarDEMO', '2019-02-22 11:25:56.143')
+SET IDENTITY_INSERT [Person].[ContactType] OFF
+SET IDENTITY_INSERT [Person].[ContactType] ON
 INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1, N'Accounting Manager', '2002-06-01 00:00:00.000')
 INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (2, N'Assistant Sales Agent', '2002-06-01 00:00:00.000')
 INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (3, N'Assistant Sales Representative', '2002-06-01 00:00:00.000')
@@ -27,4 +30,14 @@ INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VAL
 INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (26, N'fRIDAYdEMO', '2017-02-17 11:17:14.437')
 INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (27, N'TeachForAmericaDemo', '2017-03-15 13:20:12.897')
 INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (28, N'091117Demo', '2017-09-11 11:14:26.990')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1028, N'100517Demo', '2017-10-05 13:54:54.400')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1029, N'KarmakDemo', '2017-12-19 09:55:44.733')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1030, N'AllstateDemo', '2018-01-30 09:23:05.020')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1031, N'RodanandFields', '2018-05-23 14:25:11.940')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1032, N'MercuryDemo', '2018-07-13 10:37:56.233')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1033, N'TeksavvyDEMO', '2018-07-16 10:20:24.490')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1034, N'0813Demo', '2018-08-13 12:12:03.660')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1035, N'CignaTraining', '2018-10-26 09:24:26.713')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1036, N'TouchBistroDEMO', '2018-10-29 09:20:59.170')
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1037, N'Demo121318', '2018-12-13 11:27:19.533')
 SET IDENTITY_INSERT [Person].[ContactType] OFF
